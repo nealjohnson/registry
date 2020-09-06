ebd5dc68d7213ea2d8d0f68ec3693b527e2d0cbf
anil <anil>
2020-09-07 01:13:44 +1000

    Registry - Created Registry microservice

---
+++ src/main/java/com/interview/registry/ServiceInstanceController.java
@@ -0,0 +1,29 @@
+package com.interview.registry;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ServiceInstanceController {
+
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    public ServiceInstanceController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @GetMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+}
---
+++ src/main/resources/kubernetes/kube-ctl-deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f name-space.yaml
+kubectl apply -f role.yaml
+kubectl apply -f role-binding.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f registry-service.yaml
